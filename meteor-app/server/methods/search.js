7f14ace9081a3359288e723b80971d4c553b8203
Udit Desai <desaiuditd@gmail.com>
2016-06-01 10:32:21 -0700

    fixes tag search & removes tag query code temporarily

--- meteor-app/server/methods/search.js
+++ meteor-app/server/methods/search.js
@@ -19,21 +19,21 @@ Meteor.methods(
 				}
 			}
 		},
-		fetchTagResults: function (tag, indices, offset =0, limit =10) {
-			if ( tag ) {
-				var query = ES.tagQueryBuilder(tag, indices, offset, limit);
-
-				var response = ES.queryExecutioner(query);
-
-				if(response.statusCode==200) {
-
-					ES.processTags(response.data);
-
-					return response.data;
-				} else {
-					throw new Meteor.Error(result.statusCode, result.data.error);
-				}
-			}
-		}
+		// fetchTagResults: function (tag, indices, offset =0, limit =10) {
+		// 	if ( tag ) {
+		// 		var query = ES.tagQueryBuilder(tag, indices, offset, limit);
+		//
+		// 		var response = ES.queryExecutioner(query);
+		//
+		// 		if(response.statusCode==200) {
+		//
+		// 			ES.processTags(response.data);
+		//
+		// 			return response.data;
+		// 		} else {
+		// 			throw new Meteor.Error(result.statusCode, result.data.error);
+		// 		}
+		// 	}
+		// }
 	}
 );
